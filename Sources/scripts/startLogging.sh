50a02c7b825b9d5b186f18a5de07ca2ea139b0e0
Juraj Vraniak <juvraniak@gmail.com>
2017-09-24 17:12:51 +0200

    Implementacia statickej analyzy zdrojovych suborov Diplomova-Praca/2

---
+++ Sources/scripts/startLogging.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Author : Bc. Juraj Vraniak
+# Name: startLogging.sh
+# Date : 12.9.2017
+# Description : Script to start logging of keystrokes.
+# version 1.0
+#
+
+sudo logkeys -s -m /home/jv/Documents/Skola/DP\ praca/Diplomova-Praca/Sources/scripts/my_lang.keymap -o /home/jv/Documents/Skola/DP\ praca/Diplomova-Praca/Sources/logs/logkeystroke.log
---
+++ Sources/scripts/stopLogging.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Author : Bc. Juraj Vraniak
+# Name: stopLogging.sh
+# Date : 12.9.2017
+# Description : Script to stop logging of keystrokes.
+# version 1.0
+#
+
+sudo logkeys -k -m /home/jv/Documents/Skola/DP\ praca/Diplomova-Praca/Sources/scripts/my_lang.keymap -o /home/jv/Documents/Skola/DP\ praca/Diplomova-Praca/Sources/logs/logkeystroke.log
